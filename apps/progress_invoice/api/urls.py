4e474ed725cb8a5ba0245a5da8f6683e63fbdc2a
Steve Nguyen <57670039+thuong-lino@users.noreply.github.com>
2022-12-03 17:45:06 +0700

    rm cached

--- apps/progress_invoice/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_allocations, put_allocation, refresh_db_view
-
-urlpatterns = [
-    path('data/', get_allocations),
-    path('data/<int:alloc_id>/', put_allocation, name='put_allocation'),
-    path('refresh_db/', refresh_db_view, name='refresh_all'),
-]
